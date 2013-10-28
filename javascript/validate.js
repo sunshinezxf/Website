55ebdca08d719740c030d4ec49134a283cfdd4b6
sunshine <sunshinezxf@hotmail.com>
2013-10-28 21:13:23 +0800

    fix some bugs
    Change-Id: I15e562955cd22ab54b226f89be41910744f1c4c6

--- javascript/validate.js
+++ javascript/validate.js
@@ -1,10 +1,10 @@
+/*username can only consist of alphabetic and number*/
 function legal_username(username) {
-	var username_pattern = new RegExp("^[a-zA-Z][a-zA-Z0-9_]+");
-	if(!username_pattern.test()){
-		window.alert("illegal!");
-	}
+	var username_pattern = new RegExp("^[a-zA-Z][a-zA-Z0-9_]+$");
+	return username_pattern.test(username);
 }
 
+/*connect to database to check whether username exists*/
 function check_username(username) {
 	if (ajax) {
 		ajax.open('GET', '../database/sign_up_process.php?username='
