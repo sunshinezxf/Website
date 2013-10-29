e8a5dd63631063e54ec336d665ad1411a068deb0
sunshine <zxfan11@software.nju.edu.cn>
2013-10-29 15:21:38 +0800

    close emulate_parameter mode and open errormode

--- database/connect.php
+++ database/connect.php
@@ -3,14 +3,16 @@ function connect() {
 	$dbms = "mysql";
 	$db_name = 'website';
 	$username = 'root';
-	$password = '006136';
+	$password = '';
 	$host = 'localhost';
 	$dsn = "$dbms:host=$host;dbname=$db_name";
 	try {
 		$pdo = new PDO ( $dsn, $username, $password );
 	} catch ( PDOException $pdoe ) {
-		die ( $pdoe->getMessage() );
+		die ( $pdoe->getMessage () );
 	}
+	$pdo->setAttribute ( PDO::ATTR_EMULATE_PREPARES, false );
+	$pdo->setAttribute ( PDO::ATTR_ERRMODE, true );
 	return $pdo;
 }
 ?>
