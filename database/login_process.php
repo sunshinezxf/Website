54c20c4c222a9d559eff9fc0aaa74e4bace9d625
sunshine <zxfan11@software.nju.edu.cn>
2013-10-29 15:30:09 +0800

    prepare statement in case there exists attack

--- database/login_process.php
+++ database/login_process.php
@@ -1,35 +1,37 @@
 <?php
 header ( "Content-type: text/html; charset=utf-8" );
 include_once './connect.php';
-if(isset($_POST['username']) && isset($_POST['password'])) {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$user = validate_user($username, $password);
-	if($user != null) {
-		$_SESSION['username'] = $user['u_username'];
-		$_SESSION['password'] = $user['u_password'];
+if (isset ( $_POST ['username'] ) && isset ( $_POST ['password'] )) {
+	$username = $_POST ['username'];
+	$password = $_POST ['password'];
+	$user = validate_user ( $username, $password );
+	if ($user != null) {
+		$_SESSION ['username'] = $user ['u_username'];
+		$_SESSION ['password'] = $user ['u_password'];
 	}
 }
 function validate_user($username, $password) {
 	$connection = connect ();
-	$sql = "select * from user where u_username='" . $username . "'";
+	$sql = "select * from user where u_username=:u_username";
 	$result = $connection->prepare ( $sql );
-	$result->execute ();
+	$result->execute ( array (
+			':u_username' => $username 
+	) );
 	$user = $result->fetch ( PDO::FETCH_ASSOC );
 	if ($user == null) {
-		echo "用户名不存在！";
-		return;
+		return "用户名不存在！";
 	}
-	$password = md5($password);
-	$sql = "select * from user where u_username='" . $username . "' and u_password='" . $password . "'";
+	$password = md5 ( $password );
+	$sql = "select * from user where u_username=:u_username and u_password=:u_password";
 	$result = $connection->prepare ( $sql );
-	$result->execute ();
+	$result->execute ( array (
+			'u_username' => $username,
+			'u_password' => $password 
+	) );
 	$user = $result->fetch ( PDO::FETCH_ASSOC );
 	if ($user == null) {
-		echo "密码错误！";
-		return;
+		return  "密码错误！";
 	}
-	echo "登录成功!";
 	return $user;
 }
 ?>
