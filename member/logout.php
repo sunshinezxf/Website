66fe9664e6b9f477619b7d16cf747e38ded8bd0d
sunshine <zxfan11@software.nju.edu.cn>
2013-11-15 23:00:37 +0800

    add logout operation

--- member/logout.php
+++ member/logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 if(isset($_SESSION['username']) || isset($_SESSION['password'])){
-	
+	unset($_SESSION['username']);
+	unset($_SESSION['password']);
+	echo "<script>window.location.href=\"../index.php\"</script>";
 }
 ?>
